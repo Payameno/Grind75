a7a31d4a3c253a21899f4189649bbbbb36c24f23
Payam Beigi <beigi.payam@gmail.com>
2022-09-05 05:06:43 +0000

    Invert binary tree solved

--- invertBinaryTree.js
+++ invertBinaryTree.js
@@ -1,5 +1,16 @@
 const invertTree = function(root) {
 
-  //invert binary tree
+  if (root) {
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+  }
+
+  return root;
     
 };
